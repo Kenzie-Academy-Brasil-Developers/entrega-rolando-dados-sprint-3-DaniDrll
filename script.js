2263b0bb9ca43a8a7f7b63f050985dfff2f17c89
Daniel Rodrigues <dani.rrluzz@gmail.com>
2021-07-15 09:37:16 -0300

    finalizado

--- script.js
+++ script.js
@@ -1,1 +1,37 @@
+let dado1 = 0
+let dado2 = 0
+let count = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ];
+let soma = 0
+ 
+ function Resultado(resul){
+	let caixa = document.createElement("div")
+	let texto = document.createTextNode(resul)
+	caixa.appendChild(texto)
+	document.body.appendChild(caixa)
+ }
+	for(let i = 0; i < 1000; i++) {
+		dado1 = Math.floor(Math.random() * 6) + 1;
+		dado2 = Math.floor(Math.random() * 6) + 1;
+
+		soma = (dado1 + dado2);
+
+		count[soma - 2] ++;
+		
+	}
+	
+	for(let i = 0; i <= 10; i++){
+		Resultado(( (`${i+2} : ${count[i]}`)))
+
+	}
+
+	for (let i = 0; i <= 10; i++){
+		let tamanho = count[i]/6
+		let elemento = document.createElement('div')
+		document.body.appendChild(elemento)
+		elemento.style.width = tamanho + '%'
+		elemento.style.height = '15px'
+		elemento.style.backgroundColor = '#ff00ff'
+		elemento.style.margin = '3px'
+		
+	     }
 
